ccd4d1b4e74fb96a9f56c67416570d029269067c
Minh <minh.seikel@gmail.com>
2022-09-30 11:47:38 -0700

    Person Class and test created and passed

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,9 +1,23 @@
 import pytest
+from viewing_party.person import Person
+# class Person:
+#     def __init__(self, name, watched, friends, subscriptions):
+#         self.name = name
+#         self.watched = watched
+#         self.friends = friends
+#         self.subscriptions = subscriptions
 
-def test_1():
+#     def add_watched(self,movie):
+#         self.watched.append(movie)
+#         return self.watched
+    
+#     def get_num_watched(self):
+#         return len(self.watched)
+def test_1_valid_input():
+    person1 = Person("Cheetah","Titanic","Lion","Hulu")
     # Arrange
-
+    assert person1.name == "Cheetah"
+    assert person1.watched == "Titanic"
     # Act
 
     # Assert
-    pass
